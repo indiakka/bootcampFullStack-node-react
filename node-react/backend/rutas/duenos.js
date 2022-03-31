2399232db3afc400330ecbf0af02f2a29beb66ae
indiakka <78439130+indiakka@users.noreply.github.com>
2022-03-31 17:01:50 +0200

    Filtrar entidades, parte 5: filtrar dueños

--- node-react/backend/rutas/duenos.js
+++ node-react/backend/rutas/duenos.js
@@ -10,6 +10,43 @@ module.exports = function duenosHandler(duenos) {
           mensaje: `dueno con indice ${data.indice} no encontradO`,
         }); // poniendo `` es un literal
       }
+      /* verifico que data.query traiga datos
+      en tipo o nombre o dueno, esto significa
+      que el request es una búsqueda */
+      if (
+        data.query &&
+        (typeof data.query.nombre !== "undefined" ||
+          data.query.apellido !== "undefined" ||
+          data.query.dni !== "undefined")
+      ) {
+        //creo un array con las llaves del objeto data query
+        const llavesQuery = Object.keys(data.query);
+        /* clono el array de duenoss que viene de reucursos  y este
+         irá guardando los resultados */
+        let respuestaDuenos = [...duenos];
+        /* recorro cada una de las llaves con el fin de filtrar
+        según los criterios de búsqueda */
+        for (const llave of llavesQuery) {
+          /* filtro el array de respuestas con el index solamente dejar
+          los objetos de duenos que cumplen con la búsqueda */
+          respuestaDuenos = respuestaDuenos.filter((_dueno) => {
+            /*  creo una expresión regular para que la búsqueda
+            devuelva el resultado aunque sea may. o min. o partes parciales
+            de una palabra poniendo el ig ej: mar de marta*/
+            const expresionRegular = new RegExp(data.query[llave], "ig");
+            /*  resultado guarda la verificación del string del criterio de 
+            búsqueda y los objetos de duenos, nos dice si el criterio está
+            o no, en el objeto de duenos que estamos evaluando en el momento */
+            const resultado = _dueno[llave].match(expresionRegular);
+            /* el resultado entrega null cuando no encuentra  el criterio de 
+            búsqueda, null es falso por lo tanto el filter ignorará el resultado
+            === null, y los que si tengan el criterio de búsqueda entran en el 
+            array de respuestaduenos  */
+            return resultado;
+          });
+        }
+        return callback(200, respuestaDuenos);
+      }
       callback(200, duenos);
     },
     post: (data, callback) => {
