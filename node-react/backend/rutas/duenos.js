9408b38e7d5e032bb43e261b1149e5308e971368
indiakka <78439130+indiakka@users.noreply.github.com>
2022-04-05 15:42:06 +0200

    Filtrar / buscar dueños

--- node-react/backend/rutas/duenos.js
+++ node-react/backend/rutas/duenos.js
@@ -1,3 +1,5 @@
+const { palabraSinAcentos } = require("../util");
+
 module.exports = function duenosHandler(duenos) {
   return {
     get: (data, callback) => {
@@ -15,36 +17,42 @@ module.exports = function duenosHandler(duenos) {
       que el request es una búsqueda */
       if (
         data.query &&
-        (typeof data.query.nombre !== "undefined" ||
-          data.query.apellido !== "undefined" ||
-          data.query.dni !== "undefined")
+        (data.query.nombre || data.query.apellido || data.query.dni)
       ) {
         //creo un array con las llaves del objeto data query
         const llavesQuery = Object.keys(data.query);
         /* clono el array de duenoss que viene de reucursos  y este
          irá guardando los resultados */
         let respuestaDuenos = [...duenos];
-        /* recorro cada una de las llaves con el fin de filtrar
-        según los criterios de búsqueda */
-        for (const llave of llavesQuery) {
-          /* filtro el array de respuestas con el index solamente dejar
+
+        /* filtro el array de respuestas con el index solamente dejar
           los objetos de duenos que cumplen con la búsqueda */
-          respuestaDuenos = respuestaDuenos.filter((_dueno) => {
+        respuestaDuenos = respuestaDuenos.filter((_dueno) => {
+          let resultado = false;
+          /* recorro cada una de las llaves con el fin de filtrar
+          según los criterios de búsqueda */
+          for (const llave of llavesQuery) {
+            const busqueda = palabraSinAcentos(data.query[llave]);
+
             /*  creo una expresión regular para que la búsqueda
             devuelva el resultado aunque sea may. o min. o partes parciales
             de una palabra poniendo el ig ej: mar de marta*/
-            const expresionRegular = new RegExp(data.query[llave], "ig");
-            /*  resultado guarda la verificación del string del criterio de 
+            const expresionRegular = new RegExp(busqueda, "ig");
+            const campoDuenoSinAcentos = palabraSinAcentos(_dueno[llave]);
+            /*  resultado guarda la verificación del string del criterio de
             búsqueda y los objetos de duenos, nos dice si el criterio está
             o no, en el objeto de duenos que estamos evaluando en el momento */
-            const resultado = _dueno[llave].match(expresionRegular);
-            /* el resultado entrega null cuando no encuentra  el criterio de 
+            resultado = campoDuenoSinAcentos.match(expresionRegular);
+            if (resultado) {
+              break;
+            }
+          }
+          /* el resultado entrega null cuando no encuentra  el criterio de
             búsqueda, null es falso por lo tanto el filter ignorará el resultado
             === null, y los que si tengan el criterio de búsqueda entran en el 
             array de respuestaduenos  */
-            return resultado;
-          });
-        }
+          return resultado;
+        });
         return callback(200, respuestaDuenos);
       }
       callback(200, duenos);
